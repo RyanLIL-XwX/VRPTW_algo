02f8465e1c1c80070b401c63c17bdcf61a6ca0ac
RyanLil-Xvx <lilryan988@gmail.com>
2024-06-17 18:15:39 +0800

    Add testcases for all the functions in the class VRPTW

--- model/VRPTW.py
+++ model/VRPTW.py
@@ -286,10 +286,10 @@ class VRPTW_model(object):
         # 可以直接通过str来判断时间大小, 因为时间字符串是按照从小到大的顺序排列的
         # time1 > time2: 说明time1的时间晚于time2的时间
         # time1 < time2: 说明time1的时间早于time2的时间
-        if ((arrive_time > receive_earliest_time) or (arrive_time < receive_latest_time)):
-            return False
-        else:
+        if ((arrive_time >= receive_earliest_time) and (arrive_time <= receive_latest_time)):
             return True
+        else:
+            return False
 
 if __name__ == "__main__":
     '''
@@ -299,34 +299,112 @@ if __name__ == "__main__":
     distance_store_update: dictionary容器储存过滤后的两点之间的距离, 仅包含仓库的距离信息: {距离: (address1, address2), ...}
     time_warehouse_leave: 仓库对特定地址的出发时间
     weight_collect: dictionary容器: {订单号: [重量, 收货地址]}
-    order_stay_period_dict: dictionary容器储存每个订单的停留时间: {收货地址: [停留时间(单位: 分钟), [订单号1, 订单号2, ...]]}
+    order_stay_period_info: dictionary容器储存每个订单的停留时间: {收货地址: [停留时间(单位: 分钟), [订单号1, 订单号2, ...]]}
     '''
-    file = input("Type the name of the file: ").strip() # strip()函数用于去除字符串两端的空格
     # 创建一个VRPTW_model对象, 并将file作为参数传入
     # 函数: load_data(), parse_data(), __str__()
+    file = input("Type the name of the file: ").strip() # strip()函数用于去除字符串两端的空格
     order_data = VRPTW_model(file)
     # print(order_data)
+        
+    # 测试所有class中的基础函数
+    def testbasic():
+        # 测试对于经纬度信息的收集
+        # 函数: collect_location_info(), calculate_distance()
+        location_collect = order_data.collect_location_info()
+        # print(location_collect)
+        distance_store = order_data.calculate_distance(location_collect)
+        # print(distance_store)
+        
+        # 测试对于仓库出发信息的收集
+        # 函数: warehouse_leave_info()
+        distance_store_update = order_data.warehouse_leave_info(distance_store)
+        # print(distance_store_update)
+        time_warehouse_leave = order_data.time_warehouse_leave("北京市昌平区南口镇陈庄村(京藏高速北侧)八达岭奥特莱斯F2")
+        # print(time_warehouse_leave)
+        
+        # 测试对于订单停留数间数据的收集
+        # 函数: collect_weight_info(), collect_stay_period_info(), calculate_stay_period()
+        weight_collect = order_data.collect_weight_info()
+        # print(weight_collect)
+        order_stay_period_info = order_data.collect_stay_period_info(weight_collect)
+        # print(order_stay_period_info)
     
-    # 测试对于经纬度信息的收集
-    # 函数: collect_location_info(), calculate_distance()
-    location_collect = order_data.collect_location_info()
-    # print(location_collect)
-    distance_store = order_data.calculate_distance(location_collect)
-    # print(distance_store)
+    # 测试所有的helper function, 除了calculate_stay_period()
+    # 函数: calculate_time(), weight_availble(), volume_availble(), calculate_arrive_time(), calculate_leave_time(), time_availble()
+    def testcase1():
+        # 测试weight_availble()
+        if (order_data.weight_availble(2.5) == True):
+            pass
+        else:
+            print("(1) weight_availble() failed.")
+        if (order_data.weight_availble(2.51) == False):
+            pass
+        else:
+            print("(2) weight_availble() failed.")
+            
+        # 测试volume_availble()
+        if (order_data.volume_availble(11.0) == True):
+            pass
+        else:
+            print("(1) volume_availble() failed.")
+        if (order_data.volume_availble(11.1) == False):
+            pass
+        else:
+            print("(2) volume_availble() failed.")
+        
+        # 测试time_availble()
+        if (order_data.time_availble("2024-01-01 08:00:00", "2024-01-01 20:00:00", "2024-01-01 08:00:00") == True):
+            pass
+        else:
+            print("(1) time_availble() failed.")
+        if (order_data.time_availble("2024-01-01 08:00:00", "2024-01-01 20:00:00", "2024-01-01 20:00:00") == True):
+            pass
+        else:
+            print("(2) time_availble() failed.")
+        if (order_data.time_availble("2024-01-01 08:00:00", "2024-01-01 20:00:00", "2024-01-01 07:59:59") == False):
+            pass
+        else:
+            print("(3) time_availble() failed.")
+        if (order_data.time_availble("2024-01-01 08:00:00", "2024-01-01 20:00:00", "2024-01-01 20:00:01") == False):
+            pass
+        else:
+            print("(4) time_availble() failed.")
+        if (order_data.time_availble("2024-01-01 08:00:00", "2024-01-01 20:00:00", "2023-01-01 15:00:00") == False):
+            pass
+        else:
+            print("(5) time_availble() failed.")
+        if (order_data.time_availble("2024-01-01 08:00:00", "2024-01-01 20:00:00", "2024-05-02 15:00:00") == False):
+            pass
+        else:
+            print("(6) time_availble() failed.")
+        
+        # 测试calculate_arrive_time()
+        if (order_data.calculate_arrive_time("2024-01-01 21:00:00", 55) == "2024-01-01 21:55:00"):
+            pass
+        else:
+            print("(1) calculate_arrive_time() failed.")
+        if (order_data.calculate_arrive_time("2024-01-01 21:00:00", 65) == "2024-01-01 22:05:00"):
+            pass
+        else:
+            print("(2) calculate_arrive_time() failed.")
+        
+        # 测试calculate_leave_time()
+        if (order_data.calculate_leave_time(60, "2024-01-01 21:00:00") == "2024-01-01 22:00:00"):
+            pass
+        else:
+            print("(1) calculate_leave_time() failed.")
+        if (order_data.calculate_leave_time(500, "2024-01-01 21:00:00") == "2024-01-02 05:20:00"):
+            pass
+        else:
+            print("(2) calculate_leave_time() failed.")
+
+    # run test cases
+    def runtest():
+        testbasic() # all pass
+        testcase1() # all pass
     
-    # 测试对于仓库出发信息的收集
-    # 函数: warehouse_leave_info()
-    distance_store_update = order_data.warehouse_leave_info(distance_store)
-    # print(distance_store_update)
-    time_warehouse_leave = order_data.time_warehouse_leave("北京市昌平区南口镇陈庄村(京藏高速北侧)八达岭奥特莱斯F2")
-    # print(time_warehouse_leave)
-    
-    # 测试对于订单停留数间数据的收集
-    # 函数: collect_weight_info(), collect_stay_period_info(), calculate_stay_period()
-    weight_collect = order_data.collect_weight_info()
-    # print(weight_collect)
-    order_stay_period_info = order_data.collect_stay_period_info(weight_collect)
-    # print(order_stay_period_info)
+    runtest()
     
     
     
